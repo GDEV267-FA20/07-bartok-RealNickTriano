c6fc1c0fe02c33f184d0af2eff0739c4c2755c50
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2020-11-07 18:30:16 -0500

    Added light to signal current turn (working on offsetting it for better indication)

---
+++ Assets/__Scripts/TurnLight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class TurnLight : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public Vector3 lightOffset = new Vector3(-1,0,0);
+
+    void Update()
+    {
+
+        transform.position = Vector3.back * 3;                                 // a
+
+
+
+        if (Bartok.CURRENT_PLAYER == null)
+        {                                 // b
+
+            return;
+
+        }
+
+
+
+        transform.position += Bartok.CURRENT_PLAYER.handSlotDef.pos;
+        transform.position += lightOffset;
+
+    }
+
+}
